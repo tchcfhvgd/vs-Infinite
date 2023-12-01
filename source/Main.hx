b021686b088025216837d19e74fdc06ab8244235
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2023-12-01 23:09:34 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -53,6 +53,7 @@ class Main extends Sprite
 	{
 		super();
 
+	        SUtil.gameCrashCheck();
 		if (stage != null)
 		{
 			init();
@@ -87,6 +88,7 @@ class Main extends Sprite
 			game.height = Math.ceil(stageHeight / game.zoom);
 		}
 	
+		SUtil.doTheCheck();
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
