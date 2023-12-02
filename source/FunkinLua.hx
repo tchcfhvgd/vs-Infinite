8224a4fc0cf4bf25f98d2bc3aef4ccec4f17a601
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2023-12-02 10:12:27 +0800

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1550,7 +1550,7 @@ class FunkinLua {
 				CustomFadeTransition.nextCamera = null;
 
 			if(PlayState.isStoryMode)
-				MusicBeatState.switchState(new StoryMenuState());
+				MusicBeatState.switchState(new MainMenuState());
 			else
 				MusicBeatState.switchState(new FreeplayState());
 
