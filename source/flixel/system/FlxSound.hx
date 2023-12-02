bca137ab924033093b2cc7505c2cb018b8a06ccb
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2023-12-02 17:14:03 +0800

    Update FlxSound.hx

--- source/flixel/system/FlxSound.hx
+++ source/flixel/system/FlxSound.hx
@@ -596,11 +596,9 @@ class FlxSound extends FlxBasic
 			@:privateAccess
 			if(_channel.__source != null)
 			{
-				#if cpp
 				@:privateAccess
 				this._channel.__source.__backend.setPitch(_pitch);
 				// trace('changing $name pitch new $_pitch');
-				#end
 			}
 		}
 	}
