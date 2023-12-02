2264e2e1f8dcba476d799f37664c1d88abe2b7bc
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2023-12-02 16:48:37 +0800

    Update AtlasFrameMaker.hx

--- source/animateatlas/AtlasFrameMaker.hx
+++ source/animateatlas/AtlasFrameMaker.hx
@@ -15,6 +15,14 @@ import flixel.graphics.frames.FlxFramesCollection;
 import flixel.graphics.frames.FlxFrame;
 import flixel.util.FlxColor;
 
+#if (desktop || android)
+import sys.FileSystem;
+import sys.io.File;
+#else
+import js.html.FileSystem;
+import js.html.File;
+#end
+
 using StringTools;
 class AtlasFrameMaker extends FlxFramesCollection
 {
